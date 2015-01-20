27b6ea45ef022a067b0f68e342628a5517a88f96
magron <Victor.Magron@inria.fr>
2015-01-20 12:07:32 +0000

    adding bounds on new epsilon variables

--- Source/Program.cs
+++ Source/Program.cs
@@ -27,11 +27,24 @@ namespace Real2Float
 
       ExecutionEngine.ResolveAndTypecheck(prog, filename, out UnusedLinearTypeChecker, out UnusedMoverTypeChecker);
 
-      FloatConverter Converter = new FloatConverter();
 
+      OldBackup Backup = new OldBackup();
+      Backup.Visit(prog);
+
+      FloatConverter Converter = new FloatConverter();
       Converter.Visit(prog);
 
+      Converter.GetResultBound(3, Backup);
+
       prog.AddTopLevelDeclarations(Converter.GetEpsilons());
+      Converter.GetFreshPrecision();
+      prog.AddTopLevelDeclarations(Converter.GetPrecisions());
+      Converter.GetAxiomPrecision(3);
+      Converter.GetEpsilonBounds(Converter.GetEpsilons());
+      foreach (Axiom axiom in Converter.GetAxioms())
+      {
+        prog.AddTopLevelDeclaration(axiom);
+      }
 
       using (TokenTextWriter writer = new TokenTextWriter("output.bpl", false)) {
         prog.Emit(writer);
@@ -39,13 +52,40 @@ namespace Real2Float
 
     }
   }
+  class OldBackup : StandardVisitor {
+
+    private Expr[] OldRhss = new Expr[1];
+
+    public Expr[] GetOldRhss() {
+      return OldRhss;
+    }
+
+    public override Cmd VisitAssignCmd(AssignCmd node)
+    {
+      if (node.Lhss.Count == 1) {
+        Expr e = node.Rhss[0];
+        OldRhss[0] = e; 
+      }
+      return base.VisitAssignCmd(node);
+    }    
+  }
 
   class FloatConverter : StandardVisitor {
 
     private List<Constant> Epsilons = new List<Constant>();
 
+    private List<Constant> Precisions = new List<Constant>();
+
+    public List<Axiom> Axioms = new List<Axiom>();
+
     public List<Constant> GetEpsilons() {
       return Epsilons;
+    }
+    public List<Constant> GetPrecisions() {
+      return Precisions;
+    }
+    public List<Axiom> GetAxioms() {
+      return Axioms;
     }
 
     public override Expr VisitNAryExpr(NAryExpr node)
@@ -68,13 +108,61 @@ namespace Real2Float
       return base.VisitNAryExpr(node);
     }
 
-    private Expr GetFreshEpsilon()
+   private int Count1 (List<Constant> l)
+   {
+     return l.Count() + 1;
+   }
+
+    private Expr GetFreshEpsilon ()
     {
       Epsilons.Add(new Constant(Token.NoToken, new TypedIdent(Token.NoToken,
-          "_eps" + Epsilons.Count(), Microsoft.Boogie.Type.Real), false));
+          "_eps" + Count1(Epsilons), Microsoft.Boogie.Type.Real), false));
       return Expr.Ident(Epsilons.Last());
     }
+    public void GetFreshPrecision () {
+      Precisions.Add(new Constant(Token.NoToken, new TypedIdent(Token.NoToken,
+          "delta", Microsoft.Boogie.Type.Real), false));
+    }
+    public void GetAxiomPrecision (int precision) {
+      Expr prec = Expr.Literal(BigDec.FromString("2e-"+precision));
+      Expr axiomExpr = Expr.Eq (Expr.Ident(Precisions.Last()),prec);
+      Axioms.Add(new Axiom(Token.NoToken, axiomExpr));
+    }
 
+    public void GetEpsilonBounds(List<Constant> terms)
+    {
+        Expr axiomExpr = BinaryTreeAnd(terms, 0, terms.Count - 1);
+        Axioms.Add(new Axiom(Token.NoToken, axiomExpr));
+    }
+    
+    public void GetResultBound(int precision, OldBackup Backup) {
+        Expr rhs = Expr.Literal(BigDec.FromString("2e-"+precision));
+        Console.WriteLine("{0}", Backup.GetOldRhss().Last());
+        Expr lhs = (Backup.GetOldRhss())[0];
+        Expr axiomExpr = Expr.Ge(lhs,rhs);
+        Axioms.Add(new Axiom(Token.NoToken, axiomExpr));
+    }
+
+    private Expr BinaryTreeAnd(List<Constant> terms, int start, int end)
+    {
+      Expr eps = Expr.Ident(terms[start]);
+      Expr delta = Expr.Ident(Precisions.Last());
+      Expr ineq = Expr.Ge(Expr.Mul(delta,delta),Expr.Mul(eps,eps));
+      Expr eps1 = Expr.Ident(terms[end]);
+      Expr ineq1 = Expr.Ge(Expr.Mul(delta,delta),Expr.Mul(eps1,eps1));
+
+        if (start > end)
+            return Expr.True;
+        if (start == end)
+            return ineq;
+        if (start + 1 == end) {
+            return Expr.And(ineq, ineq1);
+        }
+        var mid = (start + end) / 2;
+        return Expr.And(BinaryTreeAnd(terms, start, mid), BinaryTreeAnd(terms, mid + 1, end));
+    }
+
+    
 
   }
 
